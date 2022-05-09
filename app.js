8b4d01ecd2e5cfd051509332f46c7a92272ccf1a
unknown <ayilarasodiq1@gmail.com>
2022-05-09 15:09:45 +0100

    Added simple auth

--- app.js
+++ app.js
@@ -2,28 +2,103 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require('path');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-app.set("view engine", "ejs");
+// app-wide middleware
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.get('/', (req,res) => {
-    res.render(__dirname + "/views/home");
+// connect to db
+async function connectToDB() {
+    try{
+        await mongoose.connect('mongodb://localhost:27017/secretsdb');
+    }catch(err){
+        console.log(`Error: ${err.message}`);
+    } 
+  }
+
+connectToDB();
+
+// User schema
+const userSchema = new mongoose.Schema({
+    email : {
+        type: String,
+        required: [1, 'Please supply email']
+    },
+
+    password : {
+        type : String,
+        required : [1, 'Please supply password']
+    }
 })
+
+// User model
+const User = mongoose.model('User', userSchema);
+
+
+
+
+// view engine
+app.set("view engine", "ejs");
+
+// Routes
+app.get('/', (req,res) => {
+    res.render("home");
+})
+
 
 app.route('/register')
     .get((req,res) => {
-    res.render(__dirname + "/views/register");
+    res.render("register");
 })
     .post((req,res) => {
-        // Handle registration,and give feedback??
+        // Get data and save
+        const email = req.body.username;
+        const password = req.body.password;
+
+        const newUser = new User({
+            email : email,
+            password :password
+        })
+
+        User.create(newUser, (err) => {
+            if(err){
+                res.send(`Oops an error occured: ${err.message}`)
+            }else{
+                res.render("secrets");
+            }
+        })
     })
 
 app.route('/login')
      .get((req,res) => {
-    res.render(__dirname + "/views/login");
+      res.render("login");
 })   
     .post((req,res) => {
-        //Handle login details,and give feedback
+        //Authentication and show secrets.js.
+        const email = req.body.username;
+        const password = req.body.password;
+    
+        User.findOne({email: email, password: password}, (err, user) => {
+            if(err){
+                res.send(err.message);
+            }
+
+            if(user){
+                res.render("secrets");
+            }else{
+                res.send('Please enter valid login details');
+            }
+        })
+    })
+
+app.route('/submit')
+    .get((req,res) =>  {
+    res.render("submit")
+    })  
+    .post((req,res) => {
+        const secret = req.body.secret;
     })
 
 
@@ -47,5 +122,6 @@ app.route('/login')
 
 
 
-const PORT = process.env.PORT || 3000;
+
+const PORT =  3000;
 app.listen(PORT, () => console.log('Server is running on port ' + PORT));
